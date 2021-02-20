a6b632df1fb5378fadb6e0c7bd5acb1a3a200178
donghangwu <donghangwu@users.noreply.github.com>
2021-02-19 23:23:43 -0800

    free_Block&Free_Inode

--- lab3a.c
+++ lab3a.c
@@ -16,19 +16,28 @@
 //file descriptor
 int image_fd;
 
-unsigned int inodes_count = 0, blocks_count = 0, log_block_size = 0;
-unsigned int blocks_per_group=0,inodes_per_group=0,inode_size=0, first_ino=0;
-
-//block size
-unsigned int block_size;
-
+//used in superblock()
 //from ext_fs.h, 
 //super block struct
 struct ext2_super_block super;
+unsigned int inodes_count = 0, blocks_count = 0, log_block_size = 0;
+unsigned int blocks_per_group=0,inodes_per_group=0,inode_size=0, first_ino=0;
+//block size
+unsigned int block_size;
+
+
+//used in group_info()
 // group descriptor
 struct ext2_group_desc group;
-
 unsigned int block_bitmap=0,inode_bitmap=0,inode_table=0;
+
+
+//used in directory_entries()
+//directory entries
+struct ext2_dir_entry dir;
+
+
+
 
 //print super block info
 void superblock()
@@ -51,17 +60,18 @@ void superblock()
 void group_info()
 {
     int start_block=0;
+    //1024 
     if(block_size==1024)
         start_block=2;
-    else
+    else//or >2048
         start_block=1;
 
     //assume only 1 group
     pread(image_fd,&group,sizeof(group),(start_block*block_size));
 
-    block_bitmap=group.bg_block_bitmap;
-    inode_bitmap=group.bg_inode_bitmap;
-    inode_table=group.bg_inode_table;
+    block_bitmap=group.bg_block_bitmap;//starting block# for free block entries
+    inode_bitmap=group.bg_inode_bitmap;//starting block# for free node entries
+    inode_table=group.bg_inode_table;//starting block# for first block of i-nodes
     int group_num=0;//always 0 since we only have 1 single group
     printf("GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n",
             group_num, blocks_count, inodes_per_group,
@@ -69,6 +79,67 @@ void group_info()
             block_bitmap, inode_bitmap, inode_table);
     
 }
+
+//print free block entries
+void free_block_entries()
+{
+    char *blocks=malloc(block_size);
+
+    pread(image_fd, blocks, block_size, block_size*block_bitmap);
+
+    for(unsigned int i=0;i<block_size/8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if((blocks[i] & (1 << j))==0)
+            {
+                printf("BFREE,%d\n",i*8+j+1);//from TA's slides
+            }
+        }
+    }
+
+    free(blocks);
+
+}
+
+void free_Inode_entry()
+{
+    char *inodes=malloc(block_size);
+
+    pread(image_fd, inodes, block_size, block_size*inode_bitmap);
+
+    for(unsigned int i=0;i<block_size/8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if((inodes[i] & (1 << j))==0)
+            {
+                printf("IFREE,%d\n",i*8+j+1);//from TA's slides
+            }
+        }
+    }
+
+    free(blocks);
+}
+
+void Inode_summary()
+{
+
+}
+
+void directory_entries()
+{
+    
+
+}
+
+void indirect_block()
+{
+
+}
+
+
+
 
 int main(int argc, char** argv)
 {
@@ -94,7 +165,9 @@ int main(int argc, char** argv)
     group_info();
 
 
+    free_block_entries();
 
+    free_Inode_entry();
 
     exit(0);
 }
